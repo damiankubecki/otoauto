ccacc4c5a1e5a70b1083cedb17f2a4de6dc17d3d
Damian Kubecki <damian_094@wp.pl>
2022-08-22 17:19:07 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'app/App';
+import { store } from 'store/store';
+import { Provider } from 'react-redux';
+
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
