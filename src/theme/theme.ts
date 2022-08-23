57f9c5e265fcdae306c9ff7853bcb271809b7143
Damian Kubecki <damian_094@wp.pl>
2022-08-23 15:08:50 +0200

    Update theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,19 +1,39 @@
+import { ITheme } from 'types/types';
+
 const theme = {
-  primary: 'white',
-  secondary: 'gray',
-  tsColor: '#3178c6',
   lightFont: 300,
   boldFont: 600,
   fontSize: {
-    xxxs: '0.8rem',
-    xxs: '1rem',
-    xs: '1.2rem',
+    xxxs: '0.9rem',
+    xxs: '1.1rem',
+    xs: '1.3rem',
     s: '1.5rem',
-    m: '1.9rem',
-    l: '2.4rem',
-    xl: '3rem',
-    xxl: '4rem',
+    m: '1.6rem',
+    l: '2rem',
+    xl: '2.5rem',
+    xxl: '3rem',
+    xxxl: '4.5rem',
   },
 };
 
-export { theme };
+const lightMode: ITheme = {
+  name: 'light',
+  primary: '#ffffff',
+  secondary: '#eee',
+  additional: '#c8c9c9',
+  blue: '#1b3fba',
+  fontColor: '#24292f',
+  ...theme,
+};
+
+const darkMode: ITheme = {
+  name: 'dark',
+  primary: '#22272e',
+  secondary: '#2d333b',
+  additional: '#c8c9c9',
+  blue: '#4b6cdc',
+  fontColor: '#cdd9e5',
+  ...theme,
+};
+
+export { lightMode, darkMode };
