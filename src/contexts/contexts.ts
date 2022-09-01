f93cd89fb854a4d643a83947364ae3587ea53890
Damian Kubecki <damian_094@wp.pl>
2022-09-01 13:08:37 +0200

    Update contexts file

--- src/contexts/contexts.ts
+++ src/contexts/contexts.ts
@@ -1,5 +1,6 @@
 import { createContext } from 'react';
-import { IThemeContext, IUserContext } from 'types/types';
+import { ICarsContext, IThemeContext, IUserContext } from 'types/types';
 
 export const ThemeContext = createContext<null | IThemeContext>(null);
 export const UserContext = createContext<null | IUserContext>(null);
+export const CarsContext = createContext<null | ICarsContext>(null);
