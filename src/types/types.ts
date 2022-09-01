bf5f18116bf2f8dd6f646e9bd14b18f0e6a3fa00
Damian Kubecki <damian_094@wp.pl>
2022-09-01 13:07:42 +0200

    Update types file

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,8 +3,10 @@ export interface ITheme {
   primary: string;
   secondary: string;
   additional: string;
-  blue: string;
+  red: string;
   fontColor: string;
+  fontColorAdditional100: string;
+  fontColorAdditional200: string;
   lightFont: number;
   boldFont: number;
   fontSize: {
@@ -20,6 +22,13 @@ export interface ITheme {
   };
 }
 
+export interface ICarData {
+  id: number;
+  mark: Mark;
+  country: string;
+  models: Model[];
+}
+
 export interface IThemeContext {
   activeTheme: ITheme;
   toggleTheme: () => void;
@@ -29,3 +38,52 @@ export interface IUserContext {
   isLogged: boolean;
   username: null | string;
 }
+
+export interface ICarsContext {
+  cars: ICarData[];
+}
+
+export interface IView {
+  name: string;
+  path: string;
+  element: JSX.Element;
+}
+
+export interface IMessage {
+  textContent: string;
+  buttonContent: string;
+  buttonFn: () => void;
+}
+
+export type Bodytype =
+  | 'auta małe'
+  | 'auta miejskie'
+  | 'coupe'
+  | 'kabriolet'
+  | 'kombi'
+  | 'kompakt'
+  | 'minivan'
+  | 'sedan'
+  | 'SUV';
+
+export type Mark = string;
+
+export type Model = string;
+
+export type YearOfProduction = number;
+
+export type Mileage = number;
+
+export type Transmission = 'manulana' | 'automatyczna';
+
+export type FuelType = 'benzyna' | 'benzyna + LPG' | 'diesel' | 'hybryda' | 'elekrtyczny';
+
+export type Cost = number;
+
+export type Color = string;
+
+export type HP = number;
+
+export type Engine = number;
+
+export type Description = string;
