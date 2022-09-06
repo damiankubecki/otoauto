c4642a14dec127878e2c1eb617daaa93beb336b9
Damian Kubecki <damian_094@wp.pl>
2022-09-06 15:20:17 +0200

    Update app template

--- src/templates/AppTemplate.tsx
+++ src/templates/AppTemplate.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { ThemeContext, UserContext } from 'contexts/contexts';
 import GlobalStyle from 'theme/GlobalStyle';
-import { theme } from 'theme/theme';
+import { IThemeContext, IUserContext } from 'types/types';
 
 interface IAppTemplate {
+  theme: IThemeContext;
+  userInfo: IUserContext;
   children: JSX.Element;
 }
 
-const AppTemplate = ({ children }: IAppTemplate) => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    {children}
+const AppTemplate = ({ theme, userInfo, children }: IAppTemplate) => (
+  <ThemeProvider theme={theme.activeTheme}>
+    <ThemeContext.Provider value={theme}>
+      <UserContext.Provider value={userInfo}>
+        <GlobalStyle />
+        {children}
+      </UserContext.Provider>
+    </ThemeContext.Provider>
   </ThemeProvider>
 );
 
