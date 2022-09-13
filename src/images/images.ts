d2b0c3236982597ff7994efca71cfd06f07bfe55
Damian Kubecki <damian_094@wp.pl>
2022-09-13 12:39:01 +0200

    Add images file

---
+++ src/images/images.ts
@@ -0,0 +1,6 @@
+import home_background_IMG from './home-bg.jpg';
+import not_found_IMG from './not-found.png';
+import not_found_dark_IMG from './not-found-dark.png';
+import user_avatar_IMG from './user-avatar.png';
+
+export { home_background_IMG, not_found_IMG, not_found_dark_IMG, user_avatar_IMG };
