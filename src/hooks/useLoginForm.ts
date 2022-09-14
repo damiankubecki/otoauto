b7fde65ece85f31c14784ad9a3933ee2d659adde
Damian Kubecki <damian_094@wp.pl>
2022-09-14 14:26:02 +0200

    Update useLoginForm hook

--- src/hooks/useLoginForm.ts
+++ src/hooks/useLoginForm.ts
@@ -2,8 +2,10 @@ import { useState, FormEvent, ChangeEventHandler } from 'react';
 import { loginUserAsync } from 'store/slices/userSlice';
 import { useAppDispatch } from 'hooks/useRedux';
 import useMessage from 'hooks/useMessage';
+import useLocalStorage from 'hooks/useLocalStorage';
 import { useNavigate } from 'react-router';
 import { routes } from 'router/routes';
+import { USER_LOCALSTORAGE } from 'config';
 
 interface IFormValues {
   username: string;
@@ -13,12 +15,10 @@ interface IFormValues {
 const useLoginForm = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
+  const { saveToLocalStorage } = useLocalStorage();
   const { MessageElement, isMessageActive, showMessage, hideMessage } = useMessage();
   const [isLoading, setLoading] = useState(false);
-  const [formValues, setFormValues] = useState<IFormValues>({
-    username: 'admin',
-    password: 'admin',
-  });
+  const [formValues, setFormValues] = useState<IFormValues>({ username: '', password: '' });
 
   const handleInputChange: ChangeEventHandler<HTMLInputElement> = e => {
     setFormValues(prevState => ({ ...prevState, [e.target.name]: e.target.value }));
@@ -28,14 +28,19 @@ const useLoginForm = () => {
 
   const submitFn = async (e: FormEvent<EventTarget>) => {
     e.preventDefault();
-
     setLoading(true);
+
     try {
       const { username, password } = formValues;
       const response = await dispatch(loginUserAsync({ username, password }));
 
+      saveToLocalStorage(USER_LOCALSTORAGE, {
+        id: response.data._id,
+        username: response.data.username,
+      });
+
       showMessage({
-        textContent: `${response}`,
+        textContent: `${response.message}`,
         buttonContent: 'Przejdź do strony głównej',
         buttonFn: () => navigate(`${routes.home}`),
       });
