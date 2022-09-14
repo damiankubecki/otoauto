76ce82dcb494478272290c5e8f90290e9dbb5117
Damian Kubecki <damian_094@wp.pl>
2022-09-14 14:26:22 +0200

    Update useLogout hook

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -1,18 +1,22 @@
 import { useEffect } from 'react';
 import { logoutUserAsync } from 'store/slices/userSlice';
 import { useAppDispatch } from 'hooks/useRedux';
+import useLocalStorage from './useLocalStorage';
 import useMessage from 'hooks/useMessage';
 import { useNavigate } from 'react-router';
 import { routes } from 'router/routes';
+import { USER_LOCALSTORAGE } from 'config';
 
 const useLogout = () => {
   const navigate = useNavigate();
+  const { saveToLocalStorage } = useLocalStorage();
   const { MessageElement, showMessage } = useMessage();
   const dispatch = useAppDispatch();
 
   const logout = async () => {
     try {
       await dispatch(logoutUserAsync());
+      saveToLocalStorage(USER_LOCALSTORAGE, {});
       navigate(`${routes.home}`);
     } catch (err) {
       showMessage({
