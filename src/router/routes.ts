6a9d6628f2820b762ddae4661c22c130bccc1982
Damian Kubecki <damian_094@wp.pl>
2022-09-14 12:35:33 +0200

    Add routes file

---
+++ src/router/routes.ts
@@ -0,0 +1,10 @@
+export const routes = {
+  home: '/',
+  login: '/zaloguj',
+  register: '/rejestracja',
+  logout: '/wyloguj',
+  offers: '/oferty',
+  offer: '/oferta',
+  addOffer: '/nowa_oferta',
+  profile: '/profil',
+};
