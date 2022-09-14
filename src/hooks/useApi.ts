884df31699907edd61bad8e6f4f48e672a038280
Damian Kubecki <damian_094@wp.pl>
2022-09-14 12:22:55 +0200

    Update useApi hook

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import { API_URL, OFFER_ENDPOINT } from 'config';
-import { ICarOffer } from 'types/types';
+import { ALL_OFFERS_ENDPOINT, API_URL, OFFER_ENDPOINT, USER_ENDPOINT } from 'config';
+import { ICarOffer, IUser } from 'types/types';
 
 const useAPI = () => {
   const getOfferById = async (id: string) => {
@@ -13,7 +13,28 @@ const useAPI = () => {
     }
   };
 
-  return { getOfferById };
+  const getOffersByUserId = async (id: string) => {
+    try {
+      console.log(`${API_URL}/${ALL_OFFERS_ENDPOINT}/${id}`);
+      const response = await axios.get(`${API_URL}/${ALL_OFFERS_ENDPOINT}/${id}`);
+      const offers = response.data as ICarOffer[];
+      return offers;
+    } catch (e) {
+      return Promise.reject('Nie udało się pobrać ofert użykownika');
+    }
+  };
+
+  const getUserById = async (id: string) => {
+    try {
+      const response = await axios.get(`${API_URL}/${USER_ENDPOINT}/${id}`);
+      const userData = response.data as IUser;
+      return userData;
+    } catch (e) {
+      return Promise.reject('Użytkownik nie istnieje');
+    }
+  };
+
+  return { getOfferById, getOffersByUserId, getUserById };
 };
 
 export default useAPI;
