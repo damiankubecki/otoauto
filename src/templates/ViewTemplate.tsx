e8f2772d8e657acdf48ac71762d68c1eae22037b
Damian Kubecki <damian_094@wp.pl>
2022-09-15 18:45:03 +0200

    Add ViewTemplate template

---
+++ src/templates/ViewTemplate.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  min-width: 550px;
+  margin: 50px auto 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+interface Props {
+  children: JSX.Element | JSX.Element[];
+}
+
+const ViewTemplate = ({ children }: Props) => <Wrapper>{children}</Wrapper>;
+
+export default ViewTemplate;
