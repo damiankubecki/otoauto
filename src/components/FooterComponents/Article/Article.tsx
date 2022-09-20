fa014bb084eaa7c38e790bd4247b6eadf69bf35f
Damian Kubecki <damian_094@wp.pl>
2022-09-20 18:49:37 +0200

    Update Footer subcomponents

---
+++ src/components/FooterComponents/Article/Article.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Wrapper, Title, ItemsContainer, Link, Text } from './ArticleElements';
+
+export interface IArticleItem {
+  content: string;
+  href?: string;
+}
+
+interface Props {
+  title: string;
+  items: IArticleItem[];
+}
+
+const FooterArticle = ({ title, items }: Props) => (
+  <Wrapper>
+    <Title>{title}</Title>
+    <ItemsContainer>
+      {items.map(item =>
+        item?.href ? (
+          <Link key={item.content} href={item.href} target="_blank">
+            {item.content}
+          </Link>
+        ) : (
+          <Text key={item.content}>{item.content}</Text>
+        )
+      )}
+    </ItemsContainer>
+  </Wrapper>
+);
+
+export default FooterArticle;
