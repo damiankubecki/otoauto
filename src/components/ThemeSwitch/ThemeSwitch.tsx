102a5452bb37359dcef07b8267ef3ed4c8b4890e
Damian Kubecki <damian_094@wp.pl>
2022-09-21 20:08:14 +0200

    Update ThemeSwitch component

--- src/components/ThemeSwitch/ThemeSwitch.tsx
+++ src/components/ThemeSwitch/ThemeSwitch.tsx
@@ -1,26 +1,12 @@
 import React, { useContext } from 'react';
-import useFontAwesome from 'hooks/useFontAwesome';
 import { ThemeContext } from 'contexts/contexts';
 import { IThemeContext } from 'types/types';
-import { Button } from './ThemeSwitchElements';
+import { Switch } from './ThemeSwitchElements';
 
 const ThemeSwitch = () => {
   const { activeTheme, toggleTheme } = useContext(ThemeContext) as IThemeContext;
-  const { Icon } = useFontAwesome();
 
-  return (
-    <>
-      {activeTheme.name === 'dark' ? (
-        <Button onClick={toggleTheme} variant="light">
-          <Icon color={activeTheme.primary} size="lg" icon={['fas', 'sun']} />
-        </Button>
-      ) : (
-        <Button onClick={toggleTheme} variant="dark">
-          <Icon color={activeTheme.primary} size="lg" icon={['fas', 'moon']} />
-        </Button>
-      )}
-    </>
-  );
+  return <Switch onClick={toggleTheme} sx={{ m: 1 }} checked={activeTheme.name === 'dark'} />;
 };
 
 export default ThemeSwitch;
