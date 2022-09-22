4e208263bf15d47ada8bfd28e91c54761b215ef0
Damian Kubecki <damian_094@wp.pl>
2022-09-22 17:39:19 +0200

    Update contexts file - add loading and message contexts

--- src/contexts/contexts.ts
+++ src/contexts/contexts.ts
@@ -1,7 +1,16 @@
 import { createContext } from 'react';
-import { ICarsContext, IOffersContext, IThemeContext, IUserContext } from 'types/types';
+import {
+  ICarsContext,
+  IOffersContext,
+  IThemeContext,
+  IUserContext,
+  IMessageContext,
+  ILoadingContext,
+} from 'types/types';
 
 export const ThemeContext = createContext<null | IThemeContext>(null);
 export const UserContext = createContext<null | IUserContext>(null);
 export const CarsContext = createContext<null | ICarsContext>(null);
 export const OffersContext = createContext<null | IOffersContext>(null);
+export const MessageContext = createContext<null | IMessageContext>(null);
+export const LoadingContext = createContext<null | ILoadingContext>(null);
