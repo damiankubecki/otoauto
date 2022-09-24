1960a351377fae902754352161c4e7a4b8926376
Damian Kubecki <damian_094@wp.pl>
2022-09-24 17:24:33 +0200

    Update images

--- src/images/images.ts
+++ src/images/images.ts
@@ -1,4 +1,4 @@
-import home_background_IMG from './home-bg.jpg';
+import home_background_IMG from './home-bg.webp';
 import not_found_IMG from './not-found.png';
 import not_found_dark_IMG from './not-found-dark.png';
 import user_avatar_IMG from './user-avatar.png';
