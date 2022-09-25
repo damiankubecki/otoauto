ea37a0a84007ab9ffbe9e85eaf87b70a324f3987
Damian Kubecki <damian_094@wp.pl>
2022-09-25 17:32:34 +0200

    Add config file

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:9000';
+export const USER_ENDPOINT = 'api/user';
+export const LOGIN_ENDPOINT = 'api/user/login';
+export const LOGOUT_ENDPOINT = 'api/user/logout';
+export const REGISTER_ENDPOINT = 'api/user/register';
+export const CARS_DATA_ENDPOINT = 'api/cars';
+export const ALL_OFFERS_ENDPOINT = 'api/offers';
+export const OFFER_ENDPOINT = 'api/offer';
+
+export const USER_LOCALSTORAGE = 'user';
+export const THEME_LOCALSTORAGE = 'theme';
